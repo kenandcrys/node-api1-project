7e076c0e210f9d7978ac455486c93a6f24cae9ac
Kenneth Fisher <kenandcrys@live.com>
2023-08-08 10:14:03 -0500

    added server.get by ID

--- api/server.js
+++ api/server.js
@@ -16,6 +16,34 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+  
+    User.findById(userId)
+      .then(user => {
+        if (user) {
+           return res.json(user);
+        } else {
+            return res.status(404).json({ message: "The user with the specified ID does not exist" });
+        }
+        
+      })
+      .catch( err => {
+        res.status(500).json({
+            message: "The users information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+  });
+
+
+
 server.use('*', (_req, res) => {
     res.status(404).json({
         message: 'not found'
